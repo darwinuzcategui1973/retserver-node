5000101080a19920fd371d0b353ff4bd2fa87297
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2020-06-05 17:50:16 -0400

    SigIn google

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
@@ -12,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// habilitar la carpeta public
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+console.log(path.resolve(__dirname, "../public"));
+
+
 
 
 // configuración global de rutas.
