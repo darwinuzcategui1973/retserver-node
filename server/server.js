2756172bfbd91f2193cf99aba7cedd5e96cc1b71
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2020-06-14 10:46:12 -0400

    Cobfiguracion de CORS en servidor para poderutilizar petencion desde otro servidores

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // habilitar la carpeta public
 
 app.use(express.static(path.resolve(__dirname, "../public")));
